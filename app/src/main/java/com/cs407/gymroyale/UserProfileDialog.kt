aac92c161f3a72d48fbb5da978f6e1e158bd9d80
chaitanyasharma <miac07580@gmail.com>
2024-12-10 22:44:21 -0600

    bio added to reply dialog

--- app/src/main/java/com/cs407/gymroyale/UserProfileDialog.kt
+++ app/src/main/java/com/cs407/gymroyale/UserProfileDialog.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 
@@ -22,18 +21,20 @@ class UserProfileDialog : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val profileImageView = view.findViewById<ImageView>(R.id.profileImageView)
         val profileNameTextView = view.findViewById<TextView>(R.id.profileNameTextView)
         val profileStatsTextView = view.findViewById<TextView>(R.id.profileStatsTextView)
+        val profileBioTextView = view.findViewById<TextView>(R.id.profileBioTextView)
         val closeButton = view.findViewById<Button>(R.id.closeButton)
 
         // Get user data passed via arguments
         val userName = arguments?.getString("userName") ?: "Unknown"
         val userStats = arguments?.getString("userStats") ?: "No stats available"
+        val userBio = arguments?.getString("userBio") ?: "No bio available"
 
         // Populate the dialog
         profileNameTextView.text = userName
         profileStatsTextView.text = userStats
+        profileBioTextView.text = userBio
 
         // Set close button action
         closeButton.setOnClickListener {
@@ -42,11 +43,12 @@ class UserProfileDialog : DialogFragment() {
     }
 
     companion object {
-        fun newInstance(userName: String, userStats: String): UserProfileDialog {
+        fun newInstance(userName: String, userStats: String, userBio: String): UserProfileDialog {
             val dialog = UserProfileDialog()
             val args = Bundle()
             args.putString("userName", userName)
             args.putString("userStats", userStats)
+            args.putString("userBio", userBio)
             dialog.arguments = args
             return dialog
         }
